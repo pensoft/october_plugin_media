cfb4a107b2e460409e24184bd91aeab3a2e56965
kkaleva <kkaleva@abv.bg>
2022-08-01 16:08:49 +0300

    published at videos

---
+++ updates/builder_table_update_pensoft_media_videos.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaVideos extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_videos', function($table)
+        {
+            $table->timestamp('published_at')->default('now()');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_videos', function($table)
+        {
+            $table->dropColumn('published_at');
+        });
+    }
+}
