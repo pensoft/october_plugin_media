e1e911feb9b48f7b2f5dc676995626035d310af4
root <root@project-dev.pensoft.local>
2023-03-10 13:42:01 +0200

    revision history fix

--- Plugin.php
+++ Plugin.php
@@ -1,8 +1,23 @@
 <?php namespace Pensoft\Media;
 
+use SaurabhDhariwal\Revisionhistory\Classes\Diff as Diff;
 use System\Classes\PluginBase;
+use System\Models\Revision as Revision;
 
 class Plugin extends PluginBase
 {
+
+    public function boot(){
+        /* Extetions for revision */
+        Revision::extend(function($model){
+            /* Revison can access to the login user */
+            $model->belongsTo['user'] = ['Backend\Models\User'];
+
+            /* Revision can use diff function */
+            $model->addDynamicMethod('getDiff', function() use ($model){
+                return Diff::toHTML(Diff::compare($model->old_value, $model->new_value));
+            });
+        });
+    }
 
 }
