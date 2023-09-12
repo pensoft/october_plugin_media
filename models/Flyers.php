9ba07a7b83caab6ae43698f1d02fea659224b52b
root <root@project-dev.pensoft.local>
2023-09-12 12:40:14 +0300

    flyers language versions

--- models/Flyers.php
+++ models/Flyers.php
@@ -8,7 +8,7 @@ use Model;
 class Flyers extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
+
 
     /**
      * @var string The database table used by the model.
@@ -21,8 +21,17 @@ class Flyers extends Model
     public $rules = [
     ];
 
+    /**
+     * @var array Attributes to be cast to JSON
+     */
+    protected $jsonable = [
+        'file_language_versions'
+    ];
+
 	public $attachOne = [
 		'flyer_image' => 'System\Models\File',
 		'file' => 'System\Models\File',
+		'file_version' => 'System\Models\File',
+		'file_print' => 'System\Models\File',
 	];
 }
---
+++ updates/builder_table_update_pensoft_media_flyers.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaFlyers extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_flyers', function($table)
+        {
+            $table->string('file_language_versions')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_flyers', function($table)
+        {
+            $table->dropColumn('file_language_versions');
+        });
+    }
+}
