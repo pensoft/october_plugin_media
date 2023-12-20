97f051bc7ae8efc106f767325d94eb9479c2bf21
root <root@project-dev.pensoft.local>
2023-12-20 16:38:38 +0200

    reorder pressreleases

--- models/Pressreleases.php
+++ models/Pressreleases.php
@@ -8,7 +8,17 @@ use Model;
 class Pressreleases extends Model
 {
     use \October\Rain\Database\Traits\Validation;
-    
+    use \October\Rain\Database\Traits\Revisionable;
+    use \October\Rain\Database\Traits\NestedTree;
+
+    public $timestamps = false;
+
+    // Add  for revisions limit
+    public $revisionableLimit = 200;
+
+    // Add for revisions on particular field
+    protected $revisionable = ["id","name","youtube_url","vimeo_url"];
+
 
     /**
      * @var string The database table used by the model.
