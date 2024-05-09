14d6d1801283f05d79e6a0669f680d1a375ab8b6
root <root@project-dev.pensoft.local>
2024-05-09 15:16:00 +0300

    webinars

--- models/Webinars.php
+++ models/Webinars.php
@@ -105,6 +105,46 @@ class Webinars extends Model
         return BackendAuth::getUser()->id;
     }
 
+
+    private function convertEmbed($url) {
+        // check if the URL is a YouTube link
+        if (preg_match('/^(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\//', $url)) {
+            // check if the URL is already an embed link
+            if (!preg_match('/^(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\/embed\/(.+)$/', $url)) {
+                // modify the URL to include the embed code
+                $id = '';
+                if (strpos($url, 'youtu.be/') !== false) {
+                    // extract video id from youtu.be short link
+                    $id = substr(strstr($url, 'youtu.be/'), 9);
+                } else {
+                    // extract video id from youtube.com link
+                    $query_string = parse_url($url, PHP_URL_QUERY);
+                    parse_str($query_string, $query_params);
+                    $id = $query_params['v'] ?? '';
+                }
+                $embed_url = 'https://www.youtube.com/embed/' . $id;
+                return $embed_url;
+            }
+        }
+        return $url;
+    }
+
+    public function beforeSave()
+    {
+        $url = $this->youtube_url;
+
+        // check if the URL is a YouTube link
+        if (preg_match('/^(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\//', $url)) {
+            // check if the URL is already an embed link
+            if (!preg_match('/^(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\/embed\/(.+)$/', $url)) {
+                // modify the URL to include the embed code
+                $embed_url = $this->convertEmbed($url);
+                $this->youtube_url = $embed_url;
+            }
+        }
+    }
+
+    
         /**
      * Add translation support to this model, if available.
      *
