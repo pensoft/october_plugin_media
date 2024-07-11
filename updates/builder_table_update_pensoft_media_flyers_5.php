d2aa953d04c13b11b1c8302679679360e77344fe
root <root@project-dev.pensoft.local>
2024-07-11 15:13:50 +0300

    flyers sort

---
+++ updates/builder_table_update_pensoft_media_flyers_5.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaFlyers5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_flyers', function($table)
+        {
+            $table->integer('sort_order')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_flyers', function($table)
+        {
+            $table->dropColumn('sort_order');
+        });
+    }
+}
---
+++ updates/builder_table_update_pensoft_media_flyers_6.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaFlyers6 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_flyers', function($table)
+        {
+            $table->integer('sort_order')->default(1)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_flyers', function($table)
+        {
+            $table->integer('sort_order')->default(null)->change();
+        });
+    }
+}
