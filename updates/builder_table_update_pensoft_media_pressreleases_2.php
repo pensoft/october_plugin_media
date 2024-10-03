f7c569e9d2aea50610cef02b3733ecb70359ab18
root <root@project-dev.pensoft.local>
2024-10-03 13:43:48 +0300

    press categories

---
+++ updates/builder_table_update_pensoft_media_pressreleases_2.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaPressreleases2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_pressreleases', function($table)
+        {
+            $table->integer('category_id')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_pressreleases', function($table)
+        {
+            $table->dropColumn('category_id');
+        });
+    }
+}
