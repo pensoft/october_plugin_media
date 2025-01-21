e6f0b2bc03238be4bdff573c64382126710adf8c
root <root@project-dev.pensoft.local>
2025-01-21 12:05:41 +0200

    podcasts module

---
+++ updates/builder_table_create_pensoft_media_podcasts.php
@@ -0,0 +1,26 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreatePensoftMediaPodcasts extends Migration
+{
+    public function up()
+    {
+        Schema::create('pensoft_media_podcasts', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->string('title');
+            $table->string('url');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('pensoft_media_podcasts');
+    }
+}
