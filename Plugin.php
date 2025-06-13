557f4f925083a53c3a4f429d2b2463fbfc44df89
root <preprint@pensoft.net>
2025-06-13 16:01:10 +0300

    register permissions

--- Plugin.php
+++ Plugin.php
@@ -32,7 +32,7 @@ class Plugin extends PluginBase
                 if (!$model instanceof \Rainlab\Location\Models\Country) {
                     return;
                 }
-                
+
                 $form->addFields([
                     'country_language' => [
                         'label' => 'Country language',
@@ -51,6 +51,120 @@ class Plugin extends PluginBase
             'Pensoft\Media\Components\PageGalleries' => 'page_galleries',
             'Pensoft\Media\Components\GalleriesList' => 'galleries_list',
             'Pensoft\Media\Components\Webinars' => 'webinars'
+        ];
+    }
+
+    public function registerPermissions()
+    {
+        return [
+            'pensoft.media.access' => [
+                'tab' => 'Media',
+                'label' => 'Manage media center'
+            ],
+        ];
+    }
+
+    public function registerNavigation()
+    {
+        return [
+            'media' => [
+                'label'       => 'Media center',
+                'url'         => \Backend::url('pensoft/media/logos'),
+                'icon'        => 'oc-icon-play-circle-o',
+                'permissions' => ['pensoft.media.*'],
+                'sideMenu' => [
+                    'side-menu-media-logos' => [
+                        'label'       => 'Logos',
+                        'url'         => \Backend::url('pensoft/media/logos'),
+                        'icon'        => 'oc-icon-file-image-o',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-flyers' => [
+                        'label'       => 'Flyers',
+                        'url'         => \Backend::url('pensoft/media/flyers'),
+                        'icon'        => 'oc-icon-image',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-categories' => [
+                        'label'       => 'Flyers Categories',
+                        'url'         => \Backend::url('pensoft/media/categories'),
+                        'icon'        => 'icon-list',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-newsletter' => [
+                        'label'       => 'Newsletter',
+                        'url'         => \Backend::url('pensoft/media/newsletter'),
+                        'icon'        => 'oc-icon-newspaper-o',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-presentations' => [
+                        'label'       => 'Presentations',
+                        'url'         => \Backend::url('pensoft/media/presentations'),
+                        'icon'        => 'oc-icon-file-powerpoint-o',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-pressreleases' => [
+                        'label'       => 'Press Releases',
+                        'url'         => \Backend::url('pensoft/media/pressreleases'),
+                        'icon'        => 'oc-icon-twitch',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-item3' => [
+                        'label'       => 'Press Releases Categories',
+                        'url'         => \Backend::url('pensoft/media/presscategories'),
+                        'icon'        => 'icon-list',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-videos' => [
+                        'label'       => 'Videos',
+                        'url'         => \Backend::url('pensoft/media/videos'),
+                        'icon'        => 'oc-icon-video-camera',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-item2' => [
+                        'label'       => 'Videos Categoriesdeos',
+                        'url'         => \Backend::url('pensoft/media/videoscategories'),
+                        'icon'        => 'icon-list',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-webinars' => [
+                        'label'       => 'Webinars',
+                        'url'         => \Backend::url('pensoft/media/webinars'),
+                        'icon'        => 'oc-icon-headphones',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-categories-webinars' => [
+                        'label'       => 'Webinars Categories',
+                        'url'         => \Backend::url('pensoft/media/webinarscategories'),
+                        'icon'        => 'icon-list',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-item' => [
+                        'label'       => 'Graphics',
+                        'url'         => \Backend::url('pensoft/media/graphics'),
+                        'icon'        => 'icon-list',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-galleries' => [
+                        'label'       => 'Galleries',
+                        'url'         => \Backend::url('pensoft/media/galleries'),
+                        'icon'        => 'oc-icon-camera',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-media-documents' => [
+                        'label'       => 'Documents',
+                        'url'         => \Backend::url('pensoft/media/documents'),
+                        'icon'        => 'icon-file',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                    'side-menu-item4' => [
+                        'label'       => 'Podcasts',
+                        'url'         => \Backend::url('pensoft/media/podcasts'),
+                        'icon'        => 'icon-microphone',
+                        'permissions' => ['pensoft.media.*'],
+                    ],
+                ]
+            ],
         ];
     }
 
