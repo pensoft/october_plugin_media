b046b51eec0ca2ffe53664c077508c8f1c305dd6
root <preprint@pensoft.net>
2025-07-14 10:57:20 +0300

    .

--- updates/builder_table_update_pensoft_media_videos_6.php
+++ updates/builder_table_update_pensoft_media_videos_6.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php namespace Pensoft\Media\Updates;
 
 use Schema;
@@ -9,10 +10,23 @@ class BuilderTableUpdatePensoftMediaVideos6 extends Migration
     public function up(): void
     {
         Schema::table('pensoft_media_videos', function(Blueprint $table)
+=======
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaVideos6 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_videos', function($table)
+>>>>>>> 9ec6671 (added short description field to videos, webinars, podcasts)
         {
             $table->text('description')->nullable();
         });
     }
+<<<<<<< HEAD
 
     public function down(): void
     {
@@ -22,3 +36,14 @@ class BuilderTableUpdatePensoftMediaVideos6 extends Migration
         });
     }
 }
+=======
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_videos', function($table)
+        {
+            $table->dropColumn('description');
+        });
+    }
+}
+>>>>>>> 9ec6671 (added short description field to videos, webinars, podcasts)
