9ec66712fc70e91bc0c703a5e2fdc7ec360bcb9f
root <preprint@pensoft.net>
2025-07-14 10:57:20 +0300

    added short description field to videos, webinars, podcasts

---
+++ updates/builder_table_update_pensoft_media_podcasts_2.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaPodcasts2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_podcasts', function($table)
+        {
+            $table->text('description')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_podcasts', function($table)
+        {
+            $table->dropColumn('description');
+        });
+    }
+}
---
+++ updates/builder_table_update_pensoft_media_webinars_5.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\Media\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftMediaWebinars5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_media_webinars', function($table)
+        {
+            $table->text('description')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_media_webinars', function($table)
+        {
+            $table->dropColumn('description');
+        });
+    }
+}
